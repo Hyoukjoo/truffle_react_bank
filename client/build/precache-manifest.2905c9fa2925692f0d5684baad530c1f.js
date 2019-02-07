4491a9c05154d8d190671fa786bc98390a6a5745
jo920208 <jo920208@gmail.com>
2019-02-07 18:55:18 +0900

    Delete server and test folder

---
+++ client/build/precache-manifest.2905c9fa2925692f0d5684baad530c1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a79f3b4a50cce4af121",
+    "url": "/static/js/main.5a79f3b4.chunk.js"
+  },
+  {
+    "revision": "b6f8241501a3d78acf65",
+    "url": "/static/js/1.b6f82415.chunk.js"
+  },
+  {
+    "revision": "5a79f3b4a50cce4af121",
+    "url": "/static/css/main.32b2add9.chunk.css"
+  },
+  {
+    "revision": "66b8be6a098b9aae8c07e8d414603d57",
+    "url": "/index.html"
+  }
+];
